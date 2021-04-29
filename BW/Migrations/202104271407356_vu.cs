76037bd28d05e874830ec85a7e7b7d5f7e51a785
ProgrammerPoltorashka <msargsyan2002@outlook.com>
2021-04-29 10:56:57 +0300

    bad

---
+++ BW/Migrations/202104271407356_vu.cs
@@ -0,0 +1,17 @@
+namespace BW.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class vu : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+           
+        }
+    }
+}
